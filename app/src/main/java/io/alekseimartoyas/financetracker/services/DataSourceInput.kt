e63c837585ab9b52a5bc0bbd5c36ecf58b8143a7
Aleksei Martoyas <am.am664@gmail.com>
2018-07-22 21:38:13 +0300

    get and add transaction added to DataSource

--- app/src/main/java/io/alekseimartoyas/financetracker/services/DataSourceInput.kt
+++ app/src/main/java/io/alekseimartoyas/financetracker/services/DataSourceInput.kt
@@ -1,4 +1,13 @@
 package io.alekseimartoyas.financetracker.services
 
+import io.alekseimartoyas.financetracker.datalayer.Transaction
+
 interface DataSourceInput {
+    fun addTransaction(transaction: Transaction)
+
+    fun addTransactions(transactions: Array<Transaction>)
+
+    fun getTransaction(): Transaction
+
+    fun getTransactions(): Array<Transaction>
 }
