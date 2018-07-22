30d4ff486b4878346a4914f8e53bc859ef5f20a2
Aleksei Martoyas <am.am664@gmail.com>
2018-07-22 15:09:26 +0300

    screen finished and settings inited

---
+++ app/src/main/java/io/alekseimartoyas/financetracker/modules/settings/router/SettingsRouter.kt
@@ -0,0 +1,4 @@
+package io.alekseimartoyas.financetracker.modules.settings.router
+
+class SettingsRouter {
+}
---
+++ app/src/main/java/io/alekseimartoyas/financetracker/modules/settings/view/SettingsOutput.kt
@@ -0,0 +1,4 @@
+package io.alekseimartoyas.financetracker.modules.settings.view
+
+interface SettingsOutput {
+}
