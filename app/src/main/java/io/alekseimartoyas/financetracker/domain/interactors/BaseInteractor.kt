b66ccc528bc6e6505dcc8bd8db960c86f1ccc47b
Aleksei Martoyas <am.am664@gmail.com>
2018-07-28 11:28:24 +0300

    base interactor release

--- app/src/main/java/io/alekseimartoyas/financetracker/domain/interactors/BaseInteractor.kt
+++ app/src/main/java/io/alekseimartoyas/financetracker/domain/interactors/BaseInteractor.kt
@@ -1,4 +1,28 @@
 package io.alekseimartoyas.financetracker.domain.interactors
 
-class BaseInteractor {
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.disposables.Disposable
+
+abstract class BaseInteractor<ResulType, ParametrType>(
+        private val jobScheduler: Scheduler,
+        private val uiScheduler: Scheduler) {
+    private var disposable: Disposable? = null
+
+    protected abstract fun buildObservable(parametr: ParametrType?): Observable<ResulType>
+
+    fun execute(parametr: ParametrType?, subscriber: (ResulType) -> Unit) {
+        disposable = buildObservable(parametr)
+                .subscribeOn(jobScheduler)
+                .observeOn(uiScheduler)
+                .subscribe { subscriber(it) }
+    }
+
+    fun execute(subscriber: (ResulType) -> Unit) {
+        execute(null, subscriber)
+    }
+
+    fun dispose() {
+        disposable?.dispose()
+    }
 }
