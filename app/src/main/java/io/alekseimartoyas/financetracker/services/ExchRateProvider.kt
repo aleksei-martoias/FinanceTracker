c1480913a7a672be62d7ec031098e5718e7a56e2
Aleksei Martoyas <am.am664@gmail.com>
2018-07-29 16:12:16 +0300

    ExchangeRate Service fixed

--- app/src/main/java/io/alekseimartoyas/financetracker/services/ExchRateProvider.kt
+++ app/src/main/java/io/alekseimartoyas/financetracker/services/ExchRateProvider.kt
@@ -4,15 +4,9 @@ import io.alekseimartoyas.financetracker.data.network.DailyResponse
 import io.alekseimartoyas.financetracker.data.network.IRetrofitReqs
 import io.alekseimartoyas.financetracker.domain.dinversion.IExchRateProviderInput
 import io.reactivex.Observable
-import io.reactivex.Scheduler
 
 class ExchRateProvider(private val retrofit: IRetrofitReqs): IExchRateProviderInput {
     override fun getExchRates(): Observable<DailyResponse> {
         return retrofit.getExchRates()
-    }
-
-    private fun parseDailyResp(data: Any) {
-//        val valute =(data as HashMap<*, *>) ["Valute"]
-        println()
     }
 }
